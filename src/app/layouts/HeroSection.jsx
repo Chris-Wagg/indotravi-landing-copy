a0ecd8aea5dfaad8558a496839d52f621d9ddab5
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-09-15 11:30:40 +0200

    header component removed and code moved to hero section

--- src/app/layouts/HeroSection.jsx
+++ src/app/layouts/HeroSection.jsx
@@ -1,12 +1,27 @@
-import HeaderBar from '../components/HeaderBar'
-
 export default function HeroSection() {
 	return (
 		<>
-			<HeaderBar />
-			{/* <div>
-				<img src='sophie-turner-unsplash.jpg' alt='' />
-			</div> */}
+			<div>
+				<img src='sophie-turner-unsplash.jpg' alt='' class='absolute' />
+				{/* Photo by <a href="https://unsplash.com/@sophie_turner?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sophie Turner</a> on <a href="https://unsplash.com/photos/LZVmvKlchM0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+
+				<section class='flex justify-between content-center px-8 py-6 relative'>
+					<div class='flex items-center text-lg font-semibold'>
+						KiwiTravel
+					</div>
+					<section class='flex gap-6 bg-gray-300 px-14 py-4 rounded-full bg-opacity-50'>
+						<div>About</div>
+						<div>Services</div>
+						<div>Tour</div>
+						<div>About</div>
+						<div>Contact</div>
+					</section>
+					<section class='flex gap-6'>
+						<button className='language-swapper'>EN/SWE</button>
+						<button class=''>Login</button>
+					</section>
+				</section>
+			</div>
 		</>
 	)
 }
