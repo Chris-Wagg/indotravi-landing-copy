88ebe69fc6126e115f096ef6a80d9daeb14619c8
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-09-28 10:43:13 +0200

    second section basic layout added

---
+++ src/app/components/SectionTwo.jsx
@@ -0,0 +1,24 @@
+export default function SectionTwo() {
+	return (
+		<>
+			<section class='max-w mt-10 mx-20'>
+				<section class='flex justify-around'>
+					<div className='w-1/2 '>
+						<div class='text-gray-400'>Best Locations</div>
+						<div class='text-4xl'>Middle Earth Tourism</div>
+					</div>
+					<div class='text-center w-1/2 text-gray-400'>
+						Explore the amazing sights and scenes of middle earth,
+						meet the locals and try not to get eaten!
+					</div>
+				</section>
+				<section class='flex justify-around gap-4'>
+					<div>This will be an image</div>
+					<div>This will be an image</div>
+					<div>This will be an image</div>
+					<div>This will be an image</div>
+				</section>
+			</section>
+		</>
+	)
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from './components/HeroSection'
 import StatsCards from './components/StatsCards'
+import SectionTwo from './components/SectionTwo'
 
 export default function Home() {
 	return (
 		<>
 			<HeroSection />
 			{/* <StatsCards /> */}
+			<SectionTwo />
 		</>
 	)
 }
