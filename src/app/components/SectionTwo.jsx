d94954e78ea6862b5cc7dfa1b20554f4e2a633a6
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-12-05 15:11:58 +0100

    images renamed

--- src/app/components/SectionTwo.jsx
+++ src/app/components/SectionTwo.jsx
@@ -1,9 +1,9 @@
 export default function SectionTwo() {
 	return (
 		<>
-			<section class='max-w mt-10 mx-20'>
-				<section class='flex justify-around'>
-					<div className='w-1/2 '>
+			<section class=' flex flex-col items-center w-4/5 mt-10 mx-20 w-full'>
+				<section class='flex justify-around items-between'>
+					<div className='w-1/2 flex flex-col'>
 						<div class='text-gray-400'>Best Locations</div>
 						<div class='text-4xl'>Middle Earth Tourism</div>
 					</div>
@@ -12,11 +12,35 @@ export default function SectionTwo() {
 						meet the locals and try not to get eaten!
 					</div>
 				</section>
-				<section class='flex justify-around gap-4'>
-					<div>This will be an image</div>
-					<div>This will be an image</div>
-					<div>This will be an image</div>
-					<div>This will be an image</div>
+				<section class='flex gap-4 flex-wrap'>
+					<div>
+						<img
+							src='brendan-miranda-unsplash.jpg'
+							alt='test'
+							class='rounded-3xl max-w-md'
+						/>
+					</div>
+					<div>
+						<img
+							src='ed-maughan-unsplash.jpg'
+							alt=''
+							class='rounded-3xl max-w-md'
+						/>
+					</div>
+					<div>
+						<img
+							src='k-b-ul-unsplash.jpg'
+							alt=''
+							class='rounded-3xl max-w-md'
+						/>
+					</div>
+					<div>
+						<img
+							src='henry-xu-unsplash.jpg'
+							alt=''
+							class='rounded-3xl max-w-md'
+						/>
+					</div>
 				</section>
 			</section>
 		</>
